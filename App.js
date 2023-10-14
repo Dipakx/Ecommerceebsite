5fb1d95c67ce5ce305dbd30869275e643b84c5a2
Dipakx <145953743+Dipakx@users.noreply.github.com>
2023-10-14 20:11:09 +0530

    App.js

--- App.js
+++ App.js
@@ -94,6 +94,116 @@ herostyle.backgroundPosition = bgposition;
 
 /* code for Hero section */
 
+/* code for feture section */
+
+document.getElementById("feture").style.marginTop = width_of_the_screen * 40 / 1536 + "px";
+document.getElementById("feture").style.paddingRight = width_of_the_screen * 20 / 1536 + "px";
+document.getElementById("feture").style.paddingLeft = width_of_the_screen * 20 / 1536 + "px";
+
+
+var febox = document.getElementsByClassName("fe-box")
+
+for (let index = 0; index < febox.length; index++) {
+    febox[index].style.marginRight = width_of_the_screen * 35 / 1536 + "px";
+    febox[index].style.marginLeft = width_of_the_screen * 13 / 1536 + "px";
+    febox[index].style.marginBottom = width_of_the_screen * 35 / 1536 + "px";
+}
+
+var feimg = document.getElementsByClassName("fi")
+
+for (let index = 0; index < febox.length; index++) {
+    feimg[index].style.height = width_of_the_screen * 50 * 2.5 / 1536 + "px";
+    feimg[index].style.width = width_of_the_screen * 50 * 2.5 / 1536 + "px";
+}
+
+var feh6 = document.getElementsByTagName("h6");
+
+for (let index = 0; index < feh6.length; index++) {
+    feh6[index].style.fontSize = width_of_the_screen * 15 / 1536 + "px";
+}
+
+document.getElementById("qaz").style.display = "none";
+
+/* code for feture section */
+
+/* code for product section */
+
+document.getElementsByClassName("fade")[0].style.marginLeft = width_of_the_screen * -200 / 1536 + "px";
+
+var plmw = document.getElementsByClassName("plm");
+
+for (let index = 0; index < plmw.length; index++) {
+    plmw[index].style.width = width_of_the_screen * 150 / 1536 + "px";
+    plmw[index].style.height = width_of_the_screen * 250 / 1536 + "px";
+    plmw[index].style.padding = width_of_the_screen * 25 / 1536 + "px";
+}
+
+var ogcf = document.getElementsByClassName("ogc");
+
+for (let index = 0; index < ogcf.length; index++) {
+    ogcf[index].style.width = width_of_the_screen * 100 / 1536 + "px";
+    ogcf[index].style.height = width_of_the_screen * 100 / 1536 + "px";
+}
+var fasf = document.getElementsByClassName("fa-star");
+
+for (let index = 0; index < fasf.length; index++) {
+    fasf[index].style.fontSize = width_of_the_screen * 15 / 1536 + "px";
+}
+
+var stare = document.getElementsByClassName("star");
+
+for (let index = 0; index < stare.length; index++) {
+    stare[index].style.marginTop = width_of_the_screen * 20 / 1536 + "px";
+}
+var clothg = document.getElementsByClassName("cloth");
+
+for (let index = 0; index < clothg.length; index++) {
+    clothg[index].style.marginTop = width_of_the_screen * 10 / 1536 + "px";
+    clothg[index].style.fontSize = width_of_the_screen * 20 / 1536 + "px";
+}
+var proceh = document.getElementsByClassName("proce");
+
+for (let index = 0; index < proceh.length; index++) {
+    proceh[index].style.marginTop = width_of_the_screen * 10 / 1536 + "px";
+}
+var ferth = document.getElementsByClassName("fa-cart-shopping cart");
+
+for (let index = 0; index < ferth.length; index++) {
+    ferth[index].style.fontSize = width_of_the_screen * 19 / 1536 + "px";
+}
+var tfdj = document.getElementsByClassName("tfd");
+
+for (let index = 0; index < tfdj.length; index++) {
+    tfdj[index].style.marginRight = width_of_the_screen * 20 / 1536 + "px";
+    tfdj[index].style.fontSize = width_of_the_screen * 19 / 1536 + "px";
+}
+
+
+$(document).ready(function () {
+    $('.fade').slick({
+        dots: false,
+        infinite: true,
+        speed: 700,
+        autoplay: false,
+        autoplaySpeed: 1000,
+        arrows: false,
+        slidesToShow: 7.6,
+        slidesToScroll: 1
+    });
+
+});
+
+document.getElementById("efgt").style.display = "none";
+
+document.getElementById("efgt").style.display = "none";
+
+
+/* code for product section */
+
+
+
+
+
 if (width_of_the_screen <= 768) {
 
     /* to set padding of the header to looks good*/
@@ -153,9 +263,11 @@ if (width_of_the_screen <= 768) {
         navbar.fontSize = width_of_the_screen / 30 + "px";
 
         /* to set the li items in small device*/
+
     })
 
-    // code for hero sectioin for small device 
+
+    /* to set the styling of the hero section in the small device*/
 
     var heroH4 = document.getElementsByTagName("h4")[0].style;
 
@@ -207,8 +319,130 @@ if (width_of_the_screen <= 768) {
     var bgposition = width_of_the_screen * 500 / 1536 + "px";
     herostyle.backgroundPosition = bgposition;
 
-    // code for hero sectioin for small device
+    /* to set the styling of the hero section in the small device*/
 
-}
 
-/* to set the styling of the li itmes in the small device*/
+    /* code for feture section in small device */
+
+    document.getElementById("feture").style.display = "none";
+
+    document.getElementById("qaz").style.display = "flex";
+
+    document.getElementById("qaz").style.padding = width_of_the_screen * 100 / 1536 + "px";
+    document.getElementById("qaz").style.marginTop = width_of_the_screen * 100 / 1536 + "px";
+    document.getElementById("qaz").style.marginBottom = width_of_the_screen * 100 / 1536 + "px";
+
+
+    var swiper = new Swiper(".mySwiper", {
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+
+
+    var febox = document.getElementsByClassName("fe-box")
+
+    for (let index = 0; index < febox.length; index++) {
+        febox[index].style.marginRight = width_of_the_screen * 35 / 1536 + "px";
+        febox[index].style.marginLeft = width_of_the_screen * 13 / 1536 + "px";
+        febox[index].style.marginBottom = width_of_the_screen * 35 / 1536 + "px";
+    }
+
+    var feimg = document.getElementsByClassName("fi")
+
+    for (let index = 0; index < febox.length; index++) {
+        feimg[index].style.height = width_of_the_screen * 50 * 7.5 / 1536 + "px";
+        feimg[index].style.width = width_of_the_screen * 50 * 7.5 / 1536 + "px";
+    }
+
+    var feh6 = document.getElementsByTagName("h6");
+
+    for (let index = 0; index < feh6.length; index++) {
+        feh6[index].style.fontSize = width_of_the_screen * 75 / 1536 + "px";
+    }
+
+    /* code for feture section in small device */
+
+    /* code for Product section in small device */
+
+    document.getElementById("wdbf").style.display = "none";
+
+    document.getElementById("efgt").style.display = "flex";
+
+    var plmw = document.getElementsByClassName("plm");
+
+    for (let index = 0; index < plmw.length; index++) {
+        plmw[index].style.width = width_of_the_screen * 150 * 2.5 / 1536 + "px";
+        plmw[index].style.height = width_of_the_screen * 250 * 2.6 / 1536 + "px";
+        plmw[index].style.padding = width_of_the_screen * 25 * 2.5 / 1536 + "px";
+    }
+
+    var ogcf = document.getElementsByClassName("ogc");
+
+    for (let index = 0; index < ogcf.length; index++) {
+        ogcf[index].style.width = width_of_the_screen * 100 * 2.5 / 1536 + "px";
+        ogcf[index].style.height = width_of_the_screen * 100 * 2.5 / 1536 + "px";
+    }
+    var fasf = document.getElementsByClassName("fa-star");
+
+    for (let index = 0; index < fasf.length; index++) {
+        fasf[index].style.fontSize = width_of_the_screen * 15 * 2.5 / 1536 + "px";
+    }
+
+    var stare = document.getElementsByClassName("star");
+
+    for (let index = 0; index < stare.length; index++) {
+        stare[index].style.marginTop = width_of_the_screen * 20 * 2.5 / 1536 + "px";
+    }
+    var clothg = document.getElementsByClassName("cloth");
+
+    for (let index = 0; index < clothg.length; index++) {
+        clothg[index].style.marginTop = width_of_the_screen * 10 * 2.5 / 1536 + "px";
+        clothg[index].style.fontSize = width_of_the_screen * 20 * 2.5 / 1536 + "px";
+    }
+    var proceh = document.getElementsByClassName("proce");
+
+    for (let index = 0; index < proceh.length; index++) {
+        proceh[index].style.marginTop = width_of_the_screen * 10 * 2.5 / 1536 + "px";
+    }
+    var ferth = document.getElementsByClassName("fa-cart-shopping cart");
+
+    for (let index = 0; index < ferth.length; index++) {
+        ferth[index].style.fontSize = width_of_the_screen * 20 * 2.5 / 1536 + "px";
+    }
+    var tfdj = document.getElementsByClassName("tfd");
+
+    for (let index = 0; index < tfdj.length; index++) {
+        tfdj[index].style.marginRight = width_of_the_screen * 20 * 2.5 / 1536 + "px";
+        tfdj[index].style.fontSize = width_of_the_screen * 19 * 2.5 / 1536 + "px";
+    }
+
+
+    $(document).ready(function () {
+        $('.efrg').slick({
+            dots: false,
+            infinite: true,
+            speed: 700,
+            autoplay: false,
+            autoplaySpeed: 1000,
+            arrows: false,
+            slidesToShow: 3.6,
+            slidesToScroll: 1
+        });
+
+    });
+
+    /* code for Product section in small device */
+
+} 
